776696731205760ce97879c7cbf50ca81206c713
hristy93 <hristy93@gmail.com>
2016-08-05 16:40:25 +0300

    Day 15: Added the core classes and some minor implementation

---
+++ ImmigrantsInvasion/ImmigrantsInvasion/RandomPropability.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmigrantsInvasion
+{
+    public sealed class RandomPropability
+    {
+        private static RandomPropability _instance = null;
+        private readonly object syncLock = new object();
+        private readonly Random _random = new Random();
+
+        private RandomPropability()
+        {
+                
+        }
+
+        public bool Propability (int propabilityValue)
+        {
+            return _random.NextDouble() >= propabilityValue;
+        }
+
+        public RandomPropability Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new RandomPropability();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
