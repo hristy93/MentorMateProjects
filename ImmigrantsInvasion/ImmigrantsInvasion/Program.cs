01d5c28d62d1938334517299ef7e799e34c56631
hristy93 <hristy93@gmail.com>
2016-08-12 13:30:49 +0300

    Day 20: Printing all the messages to a file

--- ImmigrantsInvasion/ImmigrantsInvasion/Program.cs
+++ ImmigrantsInvasion/ImmigrantsInvasion/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,39 @@ namespace ImmigrantsInvasion
     {
         static void Main(string[] args)
         {
-            Demo demo = new Demo(100, 5, 200);
-            demo.DisplayImmigrantsStatistics();
-            demo.ImmigrateAll();
-            demo.UnleashImmigrantsKillingSpree();
+            bool toPrintToFile = false;
 
-            Console.ReadLine();
+            if (toPrintToFile)
+            {
+                PrintToFile();
+            }
+            else
+            {
+                Demo demo = new Demo(100, 5, 200);
+                demo.DisplayImmigrantsStatistics();
+                demo.ImmigrateAll();
+                demo.UnleashImmigrantsKillingSpree();
+
+                Console.ReadLine();
+            }           
+        }
+
+        static void PrintToFile()
+        {
+            using (FileStream fs = new FileStream("ImmigrantsInvasionLog.txt", FileMode.Create))
+            {
+                TextWriter tmp = Console.Out;
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    Console.SetOut(sw);
+                    Demo demo = new Demo(100, 5, 200);
+                    demo.DisplayImmigrantsStatistics();
+                    demo.ImmigrateAll();
+                    demo.UnleashImmigrantsKillingSpree();
+                }
+
+                Console.SetOut(tmp);
+            }
         }
     }
 }
