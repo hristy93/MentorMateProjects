f65cfadb299f1bf96d857e7588e434e3f93288e2
hristy93 <hristy93@gmail.com>
2016-08-31 19:18:44 +0300

    Day 33: Added some layouts and activities and integrated the PCL in the project

---
+++ RaysHotDogs/RaysHotDogs/HotDogDetailActivity.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.Utility;
+
+namespace RaysHotDogs
+{
+    [Activity(Label = "Hotdog details", MainLauncher = true)]
+    public class HotDogDetailActivity : Activity
+    {
+        private ImageView hotDogImageView;
+        private TextView hotDogNameTextView;
+        private TextView shortDescriptionTextView;
+        private TextView descriptionTextView;
+        private TextView priceTextView;
+        private EditText amountEditText;
+        private Button cancelButton;
+        private Button orderButton;
+
+        private HotDog selectedHotDog;
+        //private HotDogDataService dataService;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.HotDogDetailView);
+
+            HotDogDataService dataService = new HotDogDataService();
+            selectedHotDog = dataService.GetHotDogById(1);
+
+            FindViews();
+
+            BindData();
+
+            HandleEvents();
+        }
+
+        private void FindViews()
+        {
+            hotDogImageView = FindViewById<ImageView>(Resource.Id.hotDogImageView);
+            hotDogNameTextView = FindViewById<TextView>(Resource.Id.hotDogNameTextView);
+            shortDescriptionTextView = FindViewById<TextView>(Resource.Id.shortDescriptionTextView);
+            descriptionTextView = FindViewById<TextView>(Resource.Id.descriptionTextView);
+            priceTextView = FindViewById<TextView>(Resource.Id.priceTextView);
+            amountEditText = FindViewById<EditText>(Resource.Id.amountEditText);
+            cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+            orderButton = FindViewById<Button>(Resource.Id.orderButton);
+        }
+
+        private void BindData()
+        {
+            hotDogNameTextView.Text = selectedHotDog.Name;
+            shortDescriptionTextView.Text = selectedHotDog.ShortDescription;
+            descriptionTextView.Text = selectedHotDog.Description;
+            priceTextView.Text = "Price: " + selectedHotDog.Price;
+
+            var imageBitmap = ImageHelper.GetImageBitmapFromUrl("http://gillcleerenpluralsight.blob.core.windows.net/files/" + selectedHotDog.ImagePath + ".jpg");
+
+            hotDogImageView.SetImageBitmap(imageBitmap);
+        }
+
+        private void HandleEvents()
+        {
+            orderButton.Click += OrderButton_Click;
+            cancelButton.Click += CancelButton_Click;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void OrderButton_Click(object sender, EventArgs e)
+        {
+            var amount = Int32.Parse(amountEditText.Text);
+
+            var dialog = new AlertDialog.Builder(this);
+            dialog.SetTitle("Confirmation");
+            dialog.SetMessage("Your hot dog has been added to your cart!");
+            dialog.Show();
+        }
+    }
+}
---
+++ RaysHotDogs/RaysHotDogs/Utility/ImageHelper.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+using System.Net;
+
+namespace RaysHotDogs.Utility
+{
+    public class ImageHelper
+    {
+        public static Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
+
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
+    }
+}
